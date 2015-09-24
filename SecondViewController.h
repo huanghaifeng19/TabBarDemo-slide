17379965c696319b600d3845729eb2f55f8159ae
henryhuang <henryhuang@eaglenet.cn>
2015-09-24 17:57:02 +0800

    实现类网易云音乐的界面功能

---
+++ SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  TabBarDemo
+//
+//  Created by henry on 15/9/23.
+//  Copyright (c) 2015年 henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "JDTabBarChildViewController.h"
+
+@interface SecondViewController : JDTabBarChildViewController
+- (IBAction)back:(id)sender;
+
+@end
---
+++ SecondViewController.m
@@ -0,0 +1,41 @@
+//
+//  SecondViewController.m
+//  TabBarDemo
+//
+//  Created by henry on 15/9/23.
+//  Copyright (c) 2015年 henry. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)back:(id)sender
+{
+    [self.menuController popViewControllerAnimated:YES];
+}
+@end
